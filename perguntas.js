1b213ea7987a641b4d5be768f77a1fbe0215b92c
BrendonZanchet <161982373+BrendonZanchet@users.noreply.github.com>
2024-10-25 16:57:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Oque é Python?',
+    'É uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Oque é uma função?',
+    'É um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'família',
+    'Qual o nome do seu irmão?',
+    'Brendon Zanchet'
+)
